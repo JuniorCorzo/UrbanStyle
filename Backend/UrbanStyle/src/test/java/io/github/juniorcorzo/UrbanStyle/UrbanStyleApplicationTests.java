6b5a3e541b173ccbb433108381d2515291e7d03d
JuniorCorzo <josedanielmmf@gmail.com>
2025-04-05 15:50:14 -0500

    feat(init): Setup initial Gradle project structure
    The following changes were made:
    - Created settings.gradle to define project name.
    - Added .gitattributes for line endings and binary files.
    - Included gradle-wrapper.jar and gradle-wrapper.properties for Gradle setup.
    - Added application.properties to set application name.
    - Created build.gradle with necessary dependencies and configurations.
    - Added .gitignore to exclude build artifacts and IDE files.
    - Implemented main application class and test class for context loading.

---
+++ Backend/UrbanStyle/src/test/java/io/github/juniorcorzo/UrbanStyle/UrbanStyleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.juniorcorzo.UrbanStyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanStyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
