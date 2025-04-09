0b6d675482c53b10c983f5a29e18429e8ec10993
JuniorCorzo <josedanielmmf@gmail.com>
2025-04-08 19:29:51 -0500

    feat: Refactor CategoriesService and ProductService to use dedicated mappers for DTO conversion

--- Backend/UrbanStyle/src/main/java/io/github/juniorcorzo/UrbanStyle/application/service/ProductService.java
+++ Backend/UrbanStyle/src/main/java/io/github/juniorcorzo/UrbanStyle/application/service/ProductService.java
@@ -2,9 +2,9 @@ package io.github.juniorcorzo.UrbanStyle.application.service;
 
 import io.github.juniorcorzo.UrbanStyle.domain.entities.ProductEntity;
 import io.github.juniorcorzo.UrbanStyle.domain.repository.ProductsRepository;
-import io.github.juniorcorzo.UrbanStyle.infrastructure.adapter.dtos.ProductDTO;
-import io.github.juniorcorzo.UrbanStyle.infrastructure.adapter.dtos.ResponseDTO;
-import org.modelmapper.ModelMapper;
+import io.github.juniorcorzo.UrbanStyle.infrastructure.adapter.dtos.common.ProductDTO;
+import io.github.juniorcorzo.UrbanStyle.infrastructure.adapter.dtos.response.ResponseDTO;
+import io.github.juniorcorzo.UrbanStyle.infrastructure.adapter.mapper.ProductMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -14,41 +14,48 @@ import java.util.List;
 @Service
 public class ProductService {
     private final ProductsRepository productsRepository;
-    private final ModelMapper modelMapper;
+    private final ProductMapper productMapper;
 
     @Autowired
-    public ProductService(ProductsRepository productsRepository) {
+    public ProductService(ProductsRepository productsRepository, ProductMapper productMapper) {
         this.productsRepository = productsRepository;
-        modelMapper = new ModelMapper();
+        this.productMapper = productMapper;
     }
 
     public ResponseDTO<ProductDTO> getAllProducts() {
-        List<ProductDTO> products = productsRepository.findAll().stream().map(product -> modelMapper.map(product, ProductDTO.class)).toList();
+        List<ProductDTO> products = this.productsRepository.findAll()
+                .stream()
+                .map(this.productMapper::toDTO)
+                .toList();
 
         return new ResponseDTO<>(HttpStatus.OK, products, "Products retrieved successfully");
     }
 
     public ResponseDTO<ProductDTO> getProductsByCategory(String categoryId) {
-        List<ProductDTO> products = productsRepository.findByCategory(categoryId).stream().map(product -> modelMapper.map(product, ProductDTO.class)).toList();
+        List<ProductDTO> products = this.productsRepository.findByCategory(categoryId)
+                .stream()
+                .map(this.productMapper::toDTO)
+                .toList();
+
         return new ResponseDTO<>(HttpStatus.OK, products, "Products retrieved successfully");
     }
 
     public ResponseDTO<ProductDTO> createProduct(ProductDTO productDTO) {
-        ProductEntity productEntity = modelMapper.map(productDTO, ProductEntity.class);
-        ProductEntity savedProduct = productsRepository.save(productEntity);
+        ProductEntity productEntity = this.productMapper.toEntity(productDTO);
+        ProductEntity savedProduct = this.productsRepository.save(productEntity);
 
-        return new ResponseDTO<>(HttpStatus.CREATED, List.of(modelMapper.map(savedProduct, ProductDTO.class)), "Product created successfully");
+        return new ResponseDTO<>(HttpStatus.CREATED, List.of(this.productMapper.toDTO(savedProduct)), "Product created successfully");
     }
 
     public ResponseDTO<ProductDTO> updateProduct(ProductDTO productDTO) {
-        ProductEntity productEntity = modelMapper.map(productDTO, ProductEntity.class);
-        ProductEntity updatedProduct = productsRepository.save(productEntity);
+        ProductEntity productEntity = this.productMapper.toEntity(productDTO);
+        ProductEntity updatedProduct = this.productsRepository.save(productEntity);
 
-        return new ResponseDTO<>(HttpStatus.OK, List.of(modelMapper.map(updatedProduct, ProductDTO.class)), "Product updated successfully");
+        return new ResponseDTO<>(HttpStatus.OK, List.of(this.productMapper.toDTO(updatedProduct)), "Product updated successfully");
     }
 
     public ResponseDTO<ProductDTO> deleteProduct(String id) {
-        productsRepository.deleteById(id);
+        this.productsRepository.deleteById(id);
         return new ResponseDTO<>(HttpStatus.OK, "Product deleted successfully");
     }
 }
