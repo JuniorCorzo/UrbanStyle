06ccd0df988f204c8a061e1acac33b44d18c3012
JuniorCorzo <josedanielmmf@gmail.com>
2025-04-22 20:03:12 -0500

    feat(mediators): Add DashboardMediator and remove TableMediator
    The following changes were made:
    - Introduced DashboardMediator to handle product and category tables.
    - Removed the obsolete TableMediator to streamline the codebase.

---
+++ Frontend/urban-style/src/lib/dashboard-mediator.ts
@@ -0,0 +1,27 @@
+import type { ITableMediator } from "@/interface/table-mediator.interface";
+import { productTable } from "./mediators/product-table.mediator";
+import { TABLE_MEDIATOR_METADATA } from "@/const/table-mediator.const";
+import { categoriesTable } from "./mediators/category-table.mediator";
+import type { FormMediator } from "@/interface/form-mediator.interface";
+import { productForm } from "./mediators/product-form.mediator";
+
+export async function DashboardMediator(
+  mediator: string
+): Promise<{ table: ITableMediator; form?: FormMediator } | undefined> {
+  const {
+    categoriesSearchParam: categories_search_param,
+    productsSearchParam: products_search_param,
+  } = TABLE_MEDIATOR_METADATA;
+
+  switch (mediator) {
+    case products_search_param:
+      return {
+        table: await productTable(),
+        form: await productForm(),
+      };
+    case categories_search_param:
+      return { table: await categoriesTable() };
+    default:
+      return undefined;
+  }
+}
