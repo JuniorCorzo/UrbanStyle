0bc5851b015ca112172caea70f00b44decf7de37
JuniorCorzo <josedanielmmf@gmail.com>
2025-04-29 18:59:11 -0500

    fix(dashboard): Update openModalEvent to include sendForm
    The following changes were made:
    - Modified openModalEvent function to accept sendForm and id parameters.
    - Updated handleUpdate function to pass sendData and id to openModalEvent.

--- Frontend/urban-style/src/lib/utils/open-modal-event.ts
+++ Frontend/urban-style/src/lib/utils/open-modal-event.ts
@@ -1,14 +1,22 @@
-import type { FormConfig } from "@/interface/form-mediator.interface";
+import type { FormConfig, SendForm } from "@/interface/form-mediator.interface";
 
 export interface OpenModalEvent {
   formData: FormConfig;
+  sendForm: SendForm;
+  id?: string;
 }
 
-export function openModalEvent(formData: FormConfig) {
+export function openModalEvent(
+  formData: FormConfig,
+  sendForm: SendForm,
+  id?: string
+) {
   window.dispatchEvent(
     new CustomEvent<OpenModalEvent>("open-modal", {
       detail: {
         formData,
+        sendForm,
+        id,
       },
     })
   );
