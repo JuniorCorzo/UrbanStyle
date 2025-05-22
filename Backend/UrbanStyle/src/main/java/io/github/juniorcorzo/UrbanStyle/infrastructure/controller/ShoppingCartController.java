88140f67d9e093dd034e3dbec98b115b652a9ed5
JuniorCorzo <josedanielmmf@gmail.com>
2025-05-22 15:56:58 -0500

    Add endpoint and logic to update product quantity in cart
    Introduced a new `/change-quantity` PATCH endpoint in `ShoppingCartController` to allow updating the quantity of specific products in the shopping cart. Refactored `ShoppingCartService` to streamline cart and product ID generation and implemented the logic for modifying product quantity in the cart.

--- Backend/UrbanStyle/src/main/java/io/github/juniorcorzo/UrbanStyle/infrastructure/controller/ShoppingCartController.java
+++ Backend/UrbanStyle/src/main/java/io/github/juniorcorzo/UrbanStyle/infrastructure/controller/ShoppingCartController.java
@@ -1,12 +1,10 @@
 package io.github.juniorcorzo.UrbanStyle.infrastructure.controller;
 
-import jakarta.websocket.server.PathParam;
-import org.springframework.web.bind.annotation.*;
-
 import io.github.juniorcorzo.UrbanStyle.application.service.ShoppingCartService;
 import io.github.juniorcorzo.UrbanStyle.infrastructure.adapter.dtos.common.ShoppingCartDTO;
 import io.github.juniorcorzo.UrbanStyle.infrastructure.adapter.dtos.response.ResponseDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/shopping-cart")
@@ -22,6 +20,15 @@ public class ShoppingCartController {
     @PostMapping("/add-product")
     public ResponseDTO<ShoppingCartDTO> createShoppingCart(@RequestBody ShoppingCartDTO insertShoppingCard) {
         return this.shoppingCardService.addProductsToCart(insertShoppingCard);
+    }
+
+    @PatchMapping("/change-quantity")
+    public ResponseDTO<ShoppingCartDTO> changeQuantityProduct(
+            @RequestParam("user-id") String userId,
+            @RequestParam("product-id") String productId,
+            @RequestParam int quantity
+    ) {
+        return this.shoppingCardService.changeQuantityProduct(userId, productId, quantity);
     }
 
     @PatchMapping("/update-product")
