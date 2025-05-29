1e16456f1bfa14aa96fe495a7c839d769231009b
JuniorCorzo <josedanielmmf@gmail.com>
2025-05-29 17:55:27 -0500

    Implement error handling in AddressService and update AddressRepository to return Optional

--- Backend/UrbanStyle/src/main/java/io/github/juniorcorzo/UrbanStyle/domain/repository/AddressRepository.java
+++ Backend/UrbanStyle/src/main/java/io/github/juniorcorzo/UrbanStyle/domain/repository/AddressRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AddressRepository extends ListCrudRepository<AddressEntity, String> {
     @Query("{ 'userId': ?0 }")
-    List<AddressEntity> findByUserId(String userId);
+    Optional<List<AddressEntity>> findByUserId(String userId);
 }
