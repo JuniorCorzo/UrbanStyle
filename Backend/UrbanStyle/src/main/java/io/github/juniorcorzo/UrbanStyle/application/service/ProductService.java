a73dcf1f90d5167335077e7bdb4cade2721bcaf4
JuniorCorzo <josedanielmmf@gmail.com>
2025-05-29 17:55:41 -0500

    Implement error handling in ProductService and update ProductsRepository to use Optional

--- Backend/UrbanStyle/src/main/java/io/github/juniorcorzo/UrbanStyle/application/service/ProductService.java
+++ Backend/UrbanStyle/src/main/java/io/github/juniorcorzo/UrbanStyle/application/service/ProductService.java
@@ -3,6 +3,10 @@ package io.github.juniorcorzo.UrbanStyle.application.service;
 import io.github.juniorcorzo.UrbanStyle.application.service.bulks.BulkProductService;
 import io.github.juniorcorzo.UrbanStyle.domain.dtos.ProductAggregationDomain;
 import io.github.juniorcorzo.UrbanStyle.domain.entities.ProductEntity;
+import io.github.juniorcorzo.UrbanStyle.domain.enums.DocumentsName;
+import io.github.juniorcorzo.UrbanStyle.domain.exceptions.DeleteDocumentFailed;
+import io.github.juniorcorzo.UrbanStyle.domain.exceptions.DocumentNotFound;
+import io.github.juniorcorzo.UrbanStyle.domain.exceptions.SaveDocumentFailed;
 import io.github.juniorcorzo.UrbanStyle.domain.repository.ProductsRepository;
 import io.github.juniorcorzo.UrbanStyle.infrastructure.adapter.dtos.common.ProductDTO;
 import io.github.juniorcorzo.UrbanStyle.infrastructure.adapter.dtos.request.ProductImagesDTO;
@@ -40,7 +44,7 @@ public class ProductService {
 
     public ResponseDTO<ProductDTO> getProductById(String id) {
         ProductEntity productResponse = this.productsRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Product not found"));
+                .orElseThrow(() -> new DocumentNotFound(DocumentsName.PRODUCT, id));
 
         return new ResponseDTO<>(
                 HttpStatus.OK,
@@ -50,11 +54,21 @@ public class ProductService {
     }
 
     public ResponseDTO<ProductAggregationDomain> groupProductsByCategories() {
-        return new ResponseDTO<>(HttpStatus.OK, this.productsRepository.groupAllByCategories(), "Product retrieved successfully");
+        List<ProductAggregationDomain> productsGroupedByCategories = this.productsRepository
+                .groupAllByCategories()
+                .orElseThrow(() -> new DocumentNotFound(DocumentsName.CATEGORY)
+                );
+
+        return new ResponseDTO<>(
+                HttpStatus.OK,
+                productsGroupedByCategories,
+                "Product retrieved successfully"
+        );
     }
 
-    public ResponseDTO<ProductDTO> getProductsByCategory(String categoryId) {
-        List<ProductDTO> products = this.productsRepository.findByCategory(categoryId)
+    public ResponseDTO<ProductDTO> getProductsByCategory(String categoryName) {
+        List<ProductDTO> products = this.productsRepository.findByCategory(categoryName)
+                .orElseThrow(() -> new DocumentNotFound(DocumentsName.CATEGORY, categoryName))
                 .stream()
                 .map(this.productMapper::toDTO)
                 .toList();
@@ -84,46 +98,73 @@ public class ProductService {
         List<String> futureImages = this.imageStorageService.sendImagesToStorage(productDTO.images());
         productEntity.setImages(futureImages);
 
-        ProductEntity savedProduct = this.productsRepository.save(productEntity);
-        return new ResponseDTO<>(
-                HttpStatus.CREATED,
-                List.of(this.productMapper.toDTO(savedProduct)),
-                "Product created successfully"
-        );
+        try {
+            ProductEntity savedProduct = this.productsRepository.save(productEntity);
+            return new ResponseDTO<>(
+                    HttpStatus.CREATED,
+                    List.of(this.productMapper.toDTO(savedProduct)),
+                    "Product created successfully"
+            );
+        } catch (Exception e) {
+            log.error("Error creating product", e);
+            throw new SaveDocumentFailed(DocumentsName.PRODUCT);
+        }
     }
 
     public ResponseDTO<ProductDTO> addImages(ProductImagesDTO productImagesDTO) {
-        List<String> keyImages = this.imageStorageService.sendImagesToStorage(productImagesDTO.images());
-        this.productsRepository.saveImagesToProduct(productImagesDTO.productId(), keyImages);
+        try {
+            List<String> keyImages = this.imageStorageService.sendImagesToStorage(productImagesDTO.images());
+            this.productsRepository.saveImagesToProduct(productImagesDTO.productId(), keyImages);
 
-        return new ResponseDTO<>(HttpStatus.OK, "Images added successfully");
+            return new ResponseDTO<>(HttpStatus.OK, "Images added successfully");
+        } catch (RuntimeException e) {
+            log.error("Failed adding images to product with id {}", productImagesDTO.productId(), e);
+            throw new SaveDocumentFailed(DocumentsName.IMAGES);
+        }
     }
 
     public ResponseDTO<ProductDTO> updateProduct(ProductDTO productDTO) {
         ProductEntity productEntity = this.productMapper.toEntity(productDTO);
-        if (this.productsRepository.findNameById(productDTO.id()).equals(productDTO.name())) {
-            ProductEntity updatedProduct = this.productsRepository.save(productEntity);
-            return new ResponseDTO<>(HttpStatus.OK, List.of(this.productMapper.toDTO(updatedProduct)), "Product updated successfully");
+        try {
+            if (this.productsRepository.findNameById(productDTO.id()).equals(productDTO.name())) {
+                ProductEntity updatedProduct = this.productsRepository.save(productEntity);
+                return new ResponseDTO<>(HttpStatus.OK, List.of(this.productMapper.toDTO(updatedProduct)), "Product updated successfully");
+            }
+
+
+            ProductDTO updatedProduct = this.bulkProductService.updateProduct(productEntity);
+            return new ResponseDTO<>(HttpStatus.OK, List.of(updatedProduct), "Product` updated successfully");
+        } catch (Exception e) {
+            log.error("Error updating product", e);
+            throw new SaveDocumentFailed(DocumentsName.PRODUCT);
         }
-
-
-        ProductDTO updatedProduct = this.bulkProductService.updateProduct(productEntity);
-        return new ResponseDTO<>(HttpStatus.OK, List.of(updatedProduct), "Product` updated successfully");
     }
 
     public ResponseDTO<ProductDTO> deleteProduct(String id) {
-        List<String> images = this.productsRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Product not found"))
-                .getImages();
+        try {
 
-        this.productsRepository.deleteById(id);
-        this.imageStorageService.deleteImagesFromStorage(images);
-        return new ResponseDTO<>(HttpStatus.OK, "Product deleted successfully");
+            List<String> images = this.productsRepository.findById(id)
+                    .orElseThrow(() -> new DocumentNotFound(DocumentsName.PRODUCT, id))
+                    .getImages();
+
+            this.productsRepository.deleteById(id);
+            this.imageStorageService.deleteImagesFromStorage(images);
+            return new ResponseDTO<>(HttpStatus.OK, "Product deleted successfully");
+        } catch (RuntimeException e) {
+            log.error("Error deleting product", e);
+            throw new DeleteDocumentFailed(DocumentsName.PRODUCT, id);
+        }
     }
 
     public ResponseDTO<ProductDTO> deleteImagesFromProduct(ProductImagesDTO productImagesDTO) {
-        this.imageStorageService.deleteImagesFromStorage(productImagesDTO.images());
-        this.productsRepository.deleteImagesFromProduct(productImagesDTO.productId(), productImagesDTO.images());
-        return new ResponseDTO<>(HttpStatus.OK, "Images deleted successfully");
+        try {
+
+            this.imageStorageService.deleteImagesFromStorage(productImagesDTO.images());
+            this.productsRepository.deleteImagesFromProduct(productImagesDTO.productId(), productImagesDTO.images());
+            return new ResponseDTO<>(HttpStatus.OK, "Images deleted successfully");
+        } catch (RuntimeException e) {
+            log.error("Error deleting images from product", e);
+            throw new DeleteDocumentFailed(DocumentsName.IMAGES, productImagesDTO.productId());
+        }
     }
 }
