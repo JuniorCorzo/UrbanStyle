a51a875f4025a2b8f8c9e5da8c0a988aff055175
JuniorCorzo <josedanielmmf@gmail.com>
2025-06-09 18:20:56 -0500

    feat(auth): Implement JwtCookieAuthHandler and JwtHeaderAuthHandler for token authentication

---
+++ Backend/UrbanStyle/src/main/java/io/github/juniorcorzo/UrbanStyle/infrastructure/security/mediator/JwtHeaderAuthHandler.java
@@ -0,0 +1,29 @@
+package io.github.juniorcorzo.UrbanStyle.infrastructure.security.mediator;
+
+import io.github.juniorcorzo.UrbanStyle.application.service.BaseAuthentication;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtHeaderAuthHandler implements AuthenticationMediator {
+    private final BaseAuthentication baseAuthentication;
+
+    @Override
+    public void authenticate(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        log.info("Processing Authentication by header for the endpoint {}", request.getRequestURI());
+        String authorizationHeader = request.getHeader("Authorization");
+        if (!authorizationHeader.startsWith("Bearer")) {
+            return;
+        }
+
+        String token = authorizationHeader.substring(7);
+        baseAuthentication.authenticate(request, response, token);
+    }
+}
