5333b8f478f29eee7729a970ae4269c94bae39f0
JuniorCorzo <josedanielmmf@gmail.com>
2025-06-10 18:04:27 -0500

    feat(report): Enhance report service with token handling
    The following changes were made:
    - Added `withCredentials` option to API calls for product and category sales.
    - Improved error handling for category sales API response.
    - Updated `reportSales` function to accept a token parameter for authorization.

--- Frontend/urban-style/src/service/report.service.ts
+++ Frontend/urban-style/src/service/report.service.ts
@@ -6,7 +6,9 @@ import axios from "axios";
 export function ReportService() {
   const productsMoreSold = async () => {
     const resultRequest: BestSeller[] = await axios
-      .get(`${PUBLIC_API_URL}/orders/products-most-sold`)
+      .get(`${PUBLIC_API_URL}/orders/products-most-sold`, {
+        withCredentials: true,
+      })
       .then((response) => {
         return (response.data as Response<BestSeller>).data;
       });
@@ -16,17 +18,29 @@ export function ReportService() {
 
   const categoriesMoreSold = async () => {
     const resultRequest: BestSeller[] = await axios
-      .get(`${PUBLIC_API_URL}/orders/categories-most-sold`)
+      .get(`${PUBLIC_API_URL}/orders/categories-most-sold`, {
+        withCredentials: true,
+      })
       .then((response) => {
+        if (response.status !== 200) throw Error(response.data);
+        console.log(response.headers);
         return (response.data as Response<BestSeller>).data;
+      })
+      .catch((err) => {
+        console.error(err);
+        throw Error(err);
       });
 
     return resultRequest;
   };
 
-  const reportSales = async () => {
+  const reportSales = async (token: string) => {
     const resultRequest: ReportSales = await axios
-      .get(`${PUBLIC_API_URL}/orders/report-sales`)
+      .get(`${PUBLIC_API_URL}/orders/report-sales`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
       .then((response) => {
         return (response.data as Response<ReportSales>).data[0];
       });
