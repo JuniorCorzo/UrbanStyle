e901901583e5aabc6e3f0794c5a4194935f7d9f3
JuniorCorzo <josedanielmmf@gmail.com>
2025-06-10 19:17:54 -0500

    feat(middleware): Refactor request validation logic
    The following changes were made:
    - Moved request validation logic into a separate function.
    - Improved error handling and session validation flow.
    - Ensured proper redirection for unauthorized access.

--- Frontend/urban-style/src/middleware.ts
+++ Frontend/urban-style/src/middleware.ts
@@ -1,5 +1,6 @@
 import { defineMiddleware } from "astro:middleware";
 import { verifyToken } from "./service/auth.service";
+import type { APIContext } from "astro";
 
 export const onRequest = defineMiddleware(async (context, next) => {
   try {
@@ -13,6 +14,33 @@ export const onRequest = defineMiddleware(async (context, next) => {
     }
   } catch (error) {
     console.error("Error validating session");
+  } finally {
+    const isRedirect = validateRequest(context);
+    if (isRedirect) return isRedirect;
   }
+
   return next();
 });
+
+function validateRequest(context: APIContext) {
+  const userPermittedRoutes = ["/perfil", "/shopping-cart", "/orders"];
+  const adminRoutes = ["/dashboard"];
+
+  const user = context.locals.user;
+  const urlRequest = context.url.pathname;
+  const isUserRoute = userPermittedRoutes.some((route) =>
+    urlRequest.startsWith(route)
+  );
+
+  const isAdminRoute = adminRoutes.some((route) =>
+    urlRequest.startsWith(route)
+  );
+
+  if (!user && (isUserRoute || isAdminRoute)) {
+    return context.redirect("/404");
+  }
+
+  if (user?.role === "ROLE_USER" && isAdminRoute) {
+    return context.redirect("/404");
+  }
+}
