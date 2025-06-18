14fdcf6d8e9e10550315b416a8c15f71812ffba1
JuniorCorzo <josedanielmmf@gmail.com>
2025-06-18 18:55:14 -0500

    Refactor authentication flow to prioritize cookie validation
    Updated `AuthenticationFilter` to prioritize authentication using cookies over headers. Added null checks and streamlined logic for handling "Authorization" headers, ensuring safer and clearer authentication processing.

--- Backend/UrbanStyle/src/main/java/io/github/juniorcorzo/UrbanStyle/infrastructure/filters/AuthenticationFilter.java
+++ Backend/UrbanStyle/src/main/java/io/github/juniorcorzo/UrbanStyle/infrastructure/filters/AuthenticationFilter.java
@@ -5,6 +5,7 @@ import io.github.juniorcorzo.UrbanStyle.infrastructure.security.mediator.JwtCook
 import io.github.juniorcorzo.UrbanStyle.infrastructure.security.mediator.JwtHeaderAuthHandler;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -23,17 +26,17 @@ public class AuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain) throws ServletException, IOException {
         AuthenticationMediator authenticationMediator;
-        String authorizationHeader = request.getHeader("Authorization");
-        if (!(authorizationHeader == null)) {
-            authenticationMediator = applicationContext.getBean(JwtHeaderAuthHandler.class);
+        Cookie[] cookies = request.getCookies();
+        boolean isPresentCookie = cookies != null && Arrays.stream(cookies).anyMatch((cookie) -> cookie.getName().equals("accessToken"));
+        if (isPresentCookie) {
+            authenticationMediator = applicationContext.getBean(JwtCookieAuthHandler.class);
             authenticationMediator.authenticate(request, response);
             filterChain.doFilter(request, response);
-
             return;
         }
 
-        authenticationMediator = applicationContext.getBean(JwtCookieAuthHandler.class);
-        authenticationMediator.authenticate(request, response);
-        filterChain.doFilter(request, response);
+            authenticationMediator = applicationContext.getBean(JwtHeaderAuthHandler.class);
+            authenticationMediator.authenticate(request, response);
+            filterChain.doFilter(request, response);
     }
 }
