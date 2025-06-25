59fe37725c4ad72607304c15933b2e110574afb1
JuniorCorzo <josedanielmmf@gmail.com>
2025-06-25 15:34:53 -0500

    feat(config): Add devToolbar configuration
    The following changes were made:
    - Added devToolbar configuration with enabled set to false.

--- Frontend/urban-style/astro.config.mjs
+++ Frontend/urban-style/astro.config.mjs
@@ -8,6 +8,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  devToolbar: {
+    enabled: false,
+  },
 
   security: {
     checkOrigin: true,
