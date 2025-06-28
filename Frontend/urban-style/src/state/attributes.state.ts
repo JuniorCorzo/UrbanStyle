9a277aaa356b962db7c362878d24a9b7e0a50f2f
JuniorCorzo <josedanielmmf@gmail.com>
2025-06-27 20:42:11 -0500

    feat(state): Add attribute and images stores
    The following changes were made:
    - Created a new state file for attributes and images.
    - Introduced `attributeStore` for managing attributes with IDs.
    - Introduced `imagesStore` for managing image states.

---
+++ Frontend/urban-style/src/state/attributes.state.ts
@@ -0,0 +1,6 @@
+import type { AttributesWithId } from "@/components/dashboard/react/components/forms/AttributeFields";
+import type { ImageState } from "@/components/react/hooks/useImages";
+import { atom } from "nanostores";
+
+export const attributeStore = atom<AttributesWithId[]>([]);
+export const imagesStore = atom<ImageState[]>([]);
