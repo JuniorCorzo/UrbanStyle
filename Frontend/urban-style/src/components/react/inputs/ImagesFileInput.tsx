d6ac9bf201f5bc7fbae5e2f7e65c6ec2f2d18bc3
JuniorCorzo <josedanielmmf@gmail.com>
2025-06-27 20:42:22 -0500

    feat(hooks): Implement useImages for image management
    The following changes were made:
    - Added useImages hook to manage image state and uploads.
    - Integrated image fetching and handling logic.
    - Updated ImagesFileInput component to utilize useImages for better state management.

--- Frontend/urban-style/src/components/react/inputs/ImagesFileInput.tsx
+++ Frontend/urban-style/src/components/react/inputs/ImagesFileInput.tsx
@@ -1,91 +1,88 @@
 import TrashIcon from "@heroicons/react/24/outline/TrashIcon";
 import bytes from "bytes";
-import { useLayoutEffect, useRef, useState, type ChangeEvent } from "react";
 import FileInput from "./FileInput";
-import { CLOUDFLARE_URL } from "@/config/env-config";
-
-interface ImageState {
-  imageName: string;
-  imageUrl: string;
-  file: File;
-}
+import type { Images } from "@/interface/product.interface";
+import LabelInput from "./LabelInput";
+import SelectInput from "./SelectInput";
+import { useImages } from "../hooks/useImages";
+import { cn } from "@/lib/cn";
 
 interface Props {
   label: string;
   name: string;
-  defaultImages?: string[];
+  defaultImages?: Images[];
 }
 
-export function ImagesFileInput({ name, defaultImages }: Props) {
-  const inputFileRef = useRef<HTMLInputElement>(null);
-  const [images, setImages] = useState<ImageState[]>();
-
-  useLayoutEffect(() => {
-    if (!defaultImages) return;
-    const imagesState = defaultImages?.map(async (image) => {
-      const imageUrl = `${CLOUDFLARE_URL}/${image}`;
-      const imageResponse = await fetch(imageUrl, {
-        mode: "cors",
-      });
-      const imageBlob = await imageResponse.blob();
-
-      const file = new File([imageBlob], image, {
-        type: imageBlob.type,
-      });
-      return { imageName: image, imageUrl, file } as ImageState;
-    });
-
-    Promise.all(imagesState).then((images) => setImages(images));
-  }, []);
-
-  const handleFiles = (event: ChangeEvent<HTMLInputElement>) => {
-    const files = event?.target?.files;
-    if (!files) return;
-    const images: ImageState[] = Array.from(files).map((file) => ({
-      imageName: file.name,
-      imageUrl: URL.createObjectURL(file),
-      file: file,
-    }));
-    setImages(images);
-  };
-
-  const handleRemoveAt = () => {
-    if (!inputFileRef.current) return;
-
-    const dataTransfer = new DataTransfer();
-    images?.map(({ file }) => dataTransfer.items.add(file));
-    inputFileRef.current.files = dataTransfer.files;
-  };
-
-  const handleDeleteImage = (urlDelete: string) => {
-    setImages(images?.filter(({ imageUrl }) => imageUrl !== urlDelete));
-    handleRemoveAt();
-  };
+export function ImagesFileInput({ name, label, defaultImages = [] }: Props) {
+  const {
+    images,
+    attributes,
+    inputFileRef,
+    handleFiles,
+    handleDeleteImage,
+    handleChangeColor,
+  } = useImages(defaultImages);
 
   return (
     <>
-      <FileInput
-        ref={inputFileRef}
-        onChange={handleFiles}
-        name={name}
-        accept="image/**"
-      />
-      <div className="w-full flex flex-col justify-center gap-5">
-        {images?.map(({ imageName, imageUrl, file }) => (
-          <div
-            className="w-full bg-accent px-5 py-3 rounded-lg flex justify-between items-center gap-5"
-            key={imageUrl}
-          >
-            <img className="size-28 aspect-square" src={imageUrl} />
-            <span className="flex flex-col gap-2">
-              <h3 className="font-medium">{imageName.split(".")[0]}</h3>
-              <span>{bytes(file.size, { unitSeparator: " " })}</span>
-            </span>
-            <span onClick={() => handleDeleteImage(imageUrl)}>
-              <TrashIcon className="size-7 cursor-pointer hover:scale-105 stroke-maroon"></TrashIcon>
-            </span>
-          </div>
-        ))}
+      <LabelInput label={label}>
+        <FileInput
+          ref={inputFileRef}
+          onChange={handleFiles}
+          name={name}
+          accept="image/**"
+          multiple
+        />
+      </LabelInput>
+      <div className="w-full flex flex-col justify-center gap-5 overflow-visible">
+        {images?.map(
+          ({ imageName, imageUrl, file, color, isUpload, isDelete }) => (
+            <div
+              className={cn(
+                "w-full flex flex-col bg-accent px-5 py-4 rounded-lg gap-3",
+                isDelete && "hidden"
+              )}
+              key={imageUrl}
+            >
+              <div className="flex justify-between items-center gap-5">
+                <img
+                  className="size-28 aspect-square rounded-md"
+                  src={imageUrl}
+                />
+                <span className="w-full max-w-56 flex flex-col gap-5">
+                  <h3 className="font-medium">{imageName.split(".")[0]}</h3>
+                  <span>{bytes(file.size, { unitSeparator: " " })}</span>
+                </span>
+                <span
+                  onClick={() => handleDeleteImage(imageUrl, isUpload)}
+                  title="Eliminar imagen"
+                >
+                  <TrashIcon className="size-7 cursor-pointer hover:scale-105 stroke-maroon" />
+                </span>
+              </div>
+              <div>
+                <LabelInput label="Asignar color">
+                  <SelectInput
+                    name={imageUrl}
+                    placeholder="Seleccione el color"
+                    onChange={(_, newColor) =>
+                      handleChangeColor(imageUrl, newColor)
+                    }
+                    value={
+                      color
+                        ? { text: color, value: color.toLowerCase() }
+                        : undefined
+                    }
+                    options={Array.from(attributes).map((color) => ({
+                      text: color,
+                      value: color.toLowerCase(),
+                    }))}
+                  />
+                </LabelInput>
+              </div>
+            </div>
+          )
+        )}
       </div>
     </>
   );
