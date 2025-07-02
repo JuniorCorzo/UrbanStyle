96bbdbcc1bd8c047a0e484c97df63ab117593c95
JuniorCorzo <josedanielmmf@gmail.com>
2025-07-02 18:54:35 -0500

    fix(prettier): Correct tabWidth setting in configuration
    The following changes were made:
    - Moved tabWidth setting to the correct position in .prettierrc.
    - Ensured consistent formatting rules for the project.

--- Frontend/urban-style/eslint.config.js
+++ Frontend/urban-style/eslint.config.js
@@ -2,15 +2,11 @@ import eslintPluginAstro from 'eslint-plugin-astro'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
-  ...eslintPluginAstro.configs.recommended,
-  {
-    rules: {
-      // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
-      'comma-dangle': ['error', 'always-multiline'],
-    },
-  },
-  globalIgnores(['.vercel/']),
+	...eslintPluginAstro.configs.recommended,
+	{
+		rules: {
+			'comma-dangle': ['error', 'always-multiline'],
+		},
+	},
+	globalIgnores(['.dist/']),
 ])
