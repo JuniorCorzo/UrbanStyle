d8078e3fc386e1bd014349632aa01f214770b00b
JuniorCorzo <josedanielmmf@gmail.com>
2025-07-05 14:38:30 -0500

    Title: feat(store): Initialize stores with empty arrays
    Description:
    
    The following changes were made:
    - Updated categoriesStore to initialize with an empty array.
    - Updated productStore and productGroupedStore to initialize with empty arrays.

--- Frontend/urban-style/src/state/categories.store.ts
+++ Frontend/urban-style/src/state/categories.store.ts
@@ -2,7 +2,7 @@ import type { Category } from '@/interface/category.interface'
 import { getAllCategories } from '@/service/categories.service'
 import { map } from 'nanostores'
 
-export const categoriesStore = map<Category[]>()
+export const categoriesStore = map<Category[]>([])
 
 export async function CategoriesStore() {
 	if (!categoriesStore.get().length) {
