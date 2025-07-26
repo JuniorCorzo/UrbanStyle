40294c218f08c2383958fc5aee9f3c0884ffc007
JuniorCorza <josedanielmmf@gmail.com>
2025-07-26 18:29:12 -0500

    feat(dashboard): Refactor dashboard components for better layout
    The following changes were made:
    - Updated grid layout in DashboardSummarySection for responsiveness.
    - Introduced SummaryStat component for better stats display.
    - Adjusted DashboardSummaryStats to use SummaryStat for rendering.
    - Enhanced DashboardTableMostSold layout for improved presentation.
    - Modified ProductSubComponent to use grid layout for images.
    - Updated TableMostSold to include conditional rendering based on row count.

--- Frontend/urban-style/src/components/dashboard/react/components/table/TableMostSold.tsx
+++ Frontend/urban-style/src/components/dashboard/react/components/table/TableMostSold.tsx
@@ -1,7 +1,12 @@
+import { cn } from '@/lib/cn'
 import { useTableMostSold } from '../../hooks/useTableMostSold'
 import { Table } from './Table'
 
 export default function TableMostSold() {
 	const { tableConfig } = useTableMostSold()
-	return <Table tableConfig={tableConfig} />
+	return (
+		<div className={cn('flex', tableConfig.getRowCount() > 4 && 'h-full items-center')}>
+			<Table tableConfig={tableConfig} />
+		</div>
+	)
 }
