ca9c187755b452d7bbc594aa4671c49dd9e8ce10
JuniorCorza <josedanielmmf@gmail.com>
2025-08-01 20:07:01 -0500

    refactor(reports): Rename and update report service methods
    The following changes were made:
    - Renamed `productsMoreSold` to `productsReport` for clarity.
    - Renamed `categoriesMoreSold` to `categoryReport` for consistency.
    - Updated response types in report methods to match new structure.
    - Changed API endpoints for sales reports to align with new routing.

--- Frontend/urban-style/src/service/report.service.ts
+++ Frontend/urban-style/src/service/report.service.ts
@@ -1,30 +1,30 @@
 import { PUBLIC_API_URL } from '@/config/env-config'
-import type { BestSeller, ReportSales } from '@/interface/report.interface'
+import type { CategoryReport, ProductReport, ReportSales } from '@/interface/report.interface'
 import type { Response } from '@/interface/response.interface'
 import axios from 'axios'
 
 export function ReportService() {
-	const productsMoreSold = async () => {
-		const resultRequest: BestSeller[] = await axios
-			.get(`${PUBLIC_API_URL}/orders/products-most-sold`, {
+	const productsReport = async () => {
+		const resultRequest: ProductReport[] = await axios
+			.get(`${PUBLIC_API_URL}/reports/product-report`, {
 				withCredentials: true,
 			})
 			.then((response) => {
-				return (response.data as Response<BestSeller>).data
+				return (response.data as Response<ProductReport>).data
 			})
 
 		return resultRequest
 	}
 
-	const categoriesMoreSold = async () => {
-		const resultRequest: BestSeller[] = await axios
-			.get(`${PUBLIC_API_URL}/orders/categories-most-sold`, {
+	const categoryReport = async () => {
+		const resultRequest: CategoryReport[] = await axios
+			.get(`${PUBLIC_API_URL}/reports/category-report`, {
 				withCredentials: true,
 			})
 			.then((response) => {
 				if (response.status !== 200) throw Error(response.data)
 				console.log(response.headers)
-				return (response.data as Response<BestSeller>).data
+				return (response.data as Response<CategoryReport>).data
 			})
 			.catch((err) => {
 				console.error(err)
@@ -36,7 +36,7 @@ export function ReportService() {
 
 	const reportSales = async (token: string) => {
 		const resultRequest: ReportSales = await axios
-			.get(`${PUBLIC_API_URL}/orders/report-sales`, {
+			.get(`${PUBLIC_API_URL}/reports/report-sales`, {
 				headers: {
 					Authorization: `Bearer ${token}`,
 				},
@@ -49,8 +49,8 @@ export function ReportService() {
 	}
 
 	return {
-		productsMoreSold,
-		categoriesMoreSold,
+		productsReport,
+		categoryReport,
 		reportSales,
 	}
 }
