c86f48254a5519d94e5eef3ac10899729553729a
JuniorCorzo <josedanielmmf@gmail.com>
2025-08-30 14:51:41 -0500

    refactor(auth): Simplify authentication service methods
    The following changes were made:
    - Renamed and refactored AuthRequest to login for clarity.
    - Updated verifyToken and logout methods to return structured results.
    - Adjusted LoginModal and UserMenu components to use the new AuthService methods.
    - Enhanced error handling in user state management.

--- Frontend/urban-style/src/state/user.state.ts
+++ Frontend/urban-style/src/state/user.state.ts
@@ -1,8 +1,11 @@
 import type { User } from '@/interface/user.interface'
-import { verifyToken } from '@/service/auth.service'
+import { AuthService } from '@/service/auth.service'
 import { atom, onMount } from 'nanostores'
 
 export const userStore = atom<User | null>(null)
 onMount(userStore, () => {
-	verifyToken().then((user) => userStore.set(user))
+	AuthService.verifyToken().then((user) => {
+		if (!user.success) throw new Error(user.error.toString())
+		userStore.set(user.data)
+	})
 })
